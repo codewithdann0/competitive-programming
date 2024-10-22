74a0a8ffe98b6d14353d046cf949a29c44a704d6
Daniel Tigistu <166249731+codewithdann0@users.noreply.github.com>
2024-10-22 12:10:59 -0700

    The RemoveDuplicates

---
+++ Remove_duplicats.py
@@ -0,0 +1,11 @@
+def removeDuplicates(self, nums) -> int:
+      if not nums:
+        return 0
+      k=1
+      for i in range(1, len(nums)):
+        if nums[i] != nums[i-1]:
+            nums[k]=nums[i]
+            k +=1
+      return k
+nums=[1,1,2]     
+removeDuplicates(nums)
